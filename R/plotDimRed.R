228547896ff9d146c8ac5add6e14b56f26155fda
Lukas Weber <lukas.weber.edu@gmail.com>
2020-12-11 22:30:16 -0500

    update plotDimRed for new SPE object structure

--- R/plotDimRed.R
+++ R/plotDimRed.R
@@ -1,86 +1,103 @@
 #' plotDimRed
-#'
+#' 
 #' Plots for spatial transcriptomics datasets.
-#'
-#' Functions to generate plots of spatial coordinates for spatial
-#' transcriptomics datasets, optionally including cluster labels.
-#'
+#' 
+#' Function to plot spatial transcriptomics data in reduced dimension
+#' coordinates.
+#' 
 #' This function generates a plot showing spatial coordinates (spots) in reduced
-#' dimension space (either PCA or UMAP). Cluster labels or ground truth labels
-#' can be shown with colors.
-#'
-#'
-#' @param spe Input object (SpatialExperiment).
-#'
-#' @param type Type of dimension reduction to use. Options are "PCA" or "UMAP".
-#'   Default = "UMAP".
-#'
-#' @param x_axis Name of column in reducedDim to use for x-axis. Default =
-#'   "UMAP1".
-#'
-#' @param y_axis Name of column in reducedDim to use for y-axis. Default =
-#'   "UMAP2".
-#'
-#' @param cluster_id Name of column in colData containing cluster IDs. To plot
-#'   without cluster labels, set "cluster_id = NULL". Default = "cluster_id".
-#'
-#' @param palette Color palette for cluster labels. Options are
-#'   "libd_layer_colors", "Okabe-Ito", or providing a vector of hex codes for a
-#'   custom palette.
-#'
-#'
+#' dimension coordinates (PCA or UMAP), with optional colors for cluster labels,
+#' ground truth labels, or other quantities.
+#' 
+#' 
+#' @param spe (SpatialExperiment) Input data object.
+#' 
+#' @param type (character) Type of reduced dimension plot. Options are "PCA" or
+#'   "UMAP". Default = "UMAP.
+#' 
+#' @param x_axis (character) Name of column in reducedDim slot containing
+#'   x-coordinates. Default = "UMAP1".
+#' 
+#' @param y_axis (character) Name of column in reducedDim slot containing
+#'   y-coordinates. Default = "UMAP2".
+#' 
+#' @param discrete (character) Name of column in colData containing discrete
+#'   labels (e.g. cluster labels or ground truth labels) to show with colors.
+#'   Default = NULL.
+#' 
+#' @param continuous (character) Name of column in colData containing continuous
+#'   values (e.g. total UMI counts) to show with colors. Default = NULL.
+#' 
+#' @param palette (character) Color palette. Options for discrete labels are
+#'   "libd_layer_colors", "Okabe-Ito", or a vector of hex codes for a custom
+#'   palette. Default = "libd_layer_colors". Options for continuous values are
+#'   "navy", or a vector of length two containing custom colors. Default =
+#'   "navy".
+#' 
+#' 
 #' @return Returns a ggplot object. Additional plot elements can be added as
-#'   ggplot elements (e.g. title, formatting).
-#'
-#'
+#'   ggplot elements (e.g. title, customized formatting, etc).
+#' 
+#' 
 #' @importFrom rlang sym "!!"
-#' @importFrom SingleCellExperiment colData reducedDim
-#' @importFrom ggplot2 ggplot aes geom_point scale_color_manual xlab ylab
-#'   ggtitle theme_bw theme element_blank
-#'
+#' @importFrom SingleCellExperiment reducedDim colData
+#' @importFrom ggplot2 ggplot aes geom_point xlab ylab ggtitle
+#'   scale_color_manual scale_color_gradient theme_bw theme element_blank
+#' 
 #' @export
-#'
+#' 
 #' @examples
 #' # to do
 #' 
-plotDimRed <- function(spe, 
-                       type = "UMAP", 
+plotDimRed <- function(spe, type = "UMAP", 
                        x_axis = "UMAP1", y_axis = "UMAP2", 
-                       cluster_id = NULL, 
-                       palette = "libd_layer_colors") {
+                       discrete = NULL, continuous = NULL, 
+                       palette = NULL, flip_xy_Visium = FALSE) {
   
   # note: using quasiquotation to allow custom variable names in ggplot ("sym" and "!!")
   
-  x_axis <- sym(x_axis)
-  y_axis <- sym(y_axis)
-  if (!is.null(cluster_id)) {
-    cluster_id <- sym(cluster_id)
-  }
+  x_axis_sym <- sym(x_axis)
+  y_axis_sym <- sym(y_axis)
+  if (!is.null(discrete)) discrete_sym <- sym(discrete)
+  if (!is.null(continuous)) continuous_sym <- sym(continuous)
   
-  if (length(palette) == 1 && palette == "libd_layer_colors") {
+  if (!is.null(palette) && palette == "libd_layer_colors") {
     palette <- c("#F0027F", "#377EB8", "#4DAF4A", "#984EA3", "#FFD700", "#FF7F00", "#1A1A1A", "#666666")
-  }
-  else if (length(palette) == 1 && palette == "Okabe-Ito") {
+  } else if (!is.null(palette) && palette == "Okabe-Ito") {
     palette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+  } else if (!is.null(palette) && palette == "navy") {
+    palette <- c("gray95", "navy")
   }
   
-  stopifnot(all(rownames(colData(spe)) == rownames(reducedDim(spe, type))))
+  df_plot <- reducedDim(spe, type)
   
-  df <- cbind(as.data.frame(colData(spe)), as.data.frame(reducedDim(spe, type)))
+  if (!is.null(discrete)) {
+    df_plot <- cbind(df_plot, colData(spe)[, discrete, drop = FALSE])
+  }
+  if (!is.null(continuous)) {
+    df_plot <- cbind(df_plot, colData(spe)[, continuous, drop = FALSE])
+  }
   
-  p <- ggplot(df, aes(x = !!x_axis, y = !!y_axis)) + 
+  df_plot <- as.data.frame(df_plot)
+  
+  p <- ggplot(df_plot, aes(x = !!x_axis_sym, y = !!y_axis_sym)) + 
     geom_point(size = 0.5) + 
     xlab(paste0(type, "1")) + 
     ylab(paste0(type, "2")) + 
-    ggtitle(paste0("Reduced dimensions (", type, ")")) + 
+    ggtitle("Reduced dimensions") + 
     theme_bw() + 
     theme(panel.grid = element_blank(), 
           axis.text = element_blank(), 
           axis.ticks = element_blank())
   
-  if (!is.null(cluster_id)) {
-    p <- p + aes(color = !!cluster_id) + 
+  if (!is.null(discrete)) {
+    p <- p + aes(color = !!discrete_sym) + 
       scale_color_manual(values = palette)
+  }
+  
+  if (!is.null(continuous)) {
+    p <- p + aes(color = !!continuous_sym) + 
+      scale_color_gradient(low = palette[1], high = palette[2])
   }
   
   p
