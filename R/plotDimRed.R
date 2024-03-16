e48480e82eca9a45ad8678b9da1da466521d815a
Lukas Weber <lmweberedu@gmail.com>
2024-03-15 20:27:11 -0400

    updates to plotDimRed rewrite from Estella Dong

--- R/plotDimRed.R
+++ R/plotDimRed.R
@@ -12,26 +12,30 @@
 #' @param spe (SpatialExperiment) Input data, assumed to be a
 #'   \code{SpatialExperiment} or \code{SingleCellExperiment} object.
 #' 
-#' @param type (character) Type of reduced dimension plot. Options can be "UMAP",
-#' "PCA", or any self-defined reduced dimension names. Default = "UMAP".
-#' 
-#' @param x_axis (character) Name of column in \code{reducedDim} containing
-#'   x-coordinates. Default = "UMAP1" or "PC1", depending on plot type.
-#' 
-#' @param y_axis (character) Name of column in \code{reducedDim} containing
-#'   y-coordinates. Default = "UMAP2" or "PC2", depending on plot type.
+#' @param plot_type (character) Type of reduced dimension plot. Options can be
+#'   "UMAP", "PCA", or any self-defined reduced dimension names. Default =
+#'   "UMAP".
 #' 
 #' @param annotate (character) Name of column in \code{colData} containing
 #'   values to annotate spots with colors, e.g. cluster labels (discrete values)
 #'   or total UMI counts (continuous values).
 #' 
-#' @param palette (character) Color palette for annotation. Options for discrete
+#' @param features (character) Name of column in \code{rowData} containing
+#'   continuous feature to plot. Default = "gene_name".
+#' 
+#' @param assay_name (character) Name of \code{assay} containing continuous
+#'   feature to plot.
+#' 
+#' @param update_colnames (logical) Whether to update column names of
+#'   \code{reducedDim} to default values. Default = TRUE.
+#' 
+#' @param pal (character) Color palette for annotation. Options for discrete
 #'   labels are "libd_layer_colors", "Okabe-Ito", or a vector of color names or
 #'   hex values. For continuous values, provide a vector of length 2 for the low
 #'   and high range, e.g. \code{c("gray90", "navy")}. Default =
 #'   \code{"libd_layer_colors"}.
 #' 
-#' @param pt.size (numeric) Point size for \code{geom_point()}. Default = 0.3.
+#' @param point_size (numeric) Point size for \code{geom_point()}. Default = 0.3.
 #' 
 #' @param text_by (character) Column name of the annotation to apply on top of 
 #' each cluster. Usually should put it the same as `annotate = `. unless you have 
@@ -48,10 +52,15 @@
 #'   ggplot elements (e.g. title, labels, formatting, etc).
 #' 
 #' 
-#' @importFrom SingleCellExperiment colData reducedDim
+#' @importFrom SingleCellExperiment reducedDimNames reducedDim colData rowData
+#' @importFrom SummarizedExperiment assay
+#' @importFrom grDevices colorRampPalette
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom scales hue_pal
 #' @importFrom ggrepel geom_text_repel
-#' @importFrom ggplot2 ggplot aes_string geom_point xlab ylab ggtitle theme_bw
-#'   theme element_blank scale_color_manual scale_color_gradient
+#' @importFrom ggplot2 ggplot aes_string geom_point xlab ylab theme_bw theme
+#'   element_blank scale_color_viridis_c scale_color_gradientn
+#'   scale_color_gradient scale_color_manual ggtitle labs guides
 #' 
 #' @export
 #' 
@@ -92,115 +101,137 @@
 #' # generate plot
 #' plotDimRed(spe, type = "UMAP", annotate = "ground_truth")
 #' 
-plotDimRed <- function(spe, 
-                       type = c("UMAP", "PCA"), 
-                       assay = "counts",
-                       annotate = NULL, palette = NULL, 
-                       pt.size = 0.3,
-                       text_by = NULL, text_by_size = 5, text_by_color = "black") {
+plotDimRed <- function(spe, plot_type = c("UMAP", "PCA"), 
+                       annotate = NULL, features = "gene_name", 
+                       assay_name = "counts", 
+                       update_colnames = TRUE, 
+                       palette = NULL, point_size = 0.3, 
+                       text_by = NULL, text_by_size = 5, 
+                       text_by_color = "black") {
   
-  # check validity of input arguments
-  if(length(type) != 1){
-    stop("Please specify reduced dimension type.")
-  }
-  # check validity of input arguments
-  if(!type %in% reducedDimNames(spe)){
-    stop("Reduced dimension 'type' does not exist in reducedDimNames(spe).")
-  }
+  # check validity of arguments
+  plot_type <- match.arg(plot_type)
+  stopifnot(plot_type %in% reducedDimNames(spe))
   
   stopifnot(is.character(annotate))
-  # no matter colnames(reducedDim()) null or not, reorganize for plotting
-  colnames(reducedDim(spe, type)) <- paste0(type, "_", 1:ncol(reducedDim(spe, type)))
-  
-  plt_df <- cbind.data.frame(colData(spe), reducedDim(spe, type))
-  
-  if(!annotate %in% c(names(plt_df), rownames(spe))){
-    stop("'annotate' should be in rownames(spe) or names(colData(spe)).")
-  }
-  # (optionally) add feature assay data to 'plt_df'
-  if(annotate %in% rownames(spe)){
-    stopifnot(is.character(assay))
-    plt_df[[annotate]] <- assay(spe, assay)[annotate, ]
+  if (!(annotate %in% c(colnames(colData(spe)), rowData(spe)[, features]))) {
+    stop("'annotate' should be the name of a column in colData or an entry in ", 
+         "the column 'features' in rowData")
   }
   
-  if(is.numeric(plt_df[[annotate]]) & is.null(palette)){
-    palette <- "blue" # for continuous feature, turn length(palette) = 0 to length(palette) = 1
+  # update colnames of reducedDims for plotting
+  if (update_colnames) {
+    colnames(reducedDim(spe, plot_type)) <- 
+      paste0(plot_type, "_", seq_len(ncol(reducedDim(spe, plot_type))))
   }
-  # accepts "libd_layer_colors" and "Okabe-Ito", or arbitrary color palette for NULL 
-  palette <- .get_pal(palette)
   
-  p <- ggplot(plt_df, aes_string(x = paste0(type, "_1"), 
-                                 y = paste0(type, "_2"), color = annotate)) + 
-    geom_point(size = pt.size) + 
-    xlab(paste0(type, "_1")) + 
-    ylab(paste0(type, "_2")) + 
-    # ggtitle("Reduced dimensions") + 
+  # data for plotting
+  df <- cbind.data.frame(colData(spe), reducedDim(spe, plot_type))
+  
+  # continuous values
+  if (annotate %in% rowData(spe)[, features]) {
+    stopifnot(is.character(assay_name))
+    ix <- which(rowData(spe)[, features] == annotate)
+    df[[annotate]] <- assay(spe, assay_name)[ix, ]
+  }
+  if ((annotate %in% colnames(colData(spe))) && 
+      (is.character(colData(spe)[, annotate]))) {
+    df[[annotate]] <- as.factor(df[[annotate]])
+  }
+  
+  # color palettes
+  if (is.numeric(df[[annotate]]) && is.null(palette)) {
+    # for continuous feature, change NULL to an arbitrary color so length(pal) == 1
+    pal <- "blue"
+  }
+  # accepts "libd_layer_colors" and "Okabe-Ito", or arbitrary color palette for NULL
+  pal <- .get_pal(pal)
+  
+  dim_labels <- colnames(reducedDim(spe, plot_type))[1:2]
+  x_label <- dim_labels[1]
+  y_label <- dim_labels[2]
+  
+  
+  # main plot
+  
+  p <- ggplot(df, aes_string(x = x_label, y = y_label, color = annotate)) + 
+    geom_point(size = point_size) + 
+    xlab(x_label) + 
+    ylab(y_label) + 
     theme_bw() + 
-    theme(panel.grid = element_blank(), 
-          #axis.text = element_blank(), 
-          #axis.ticks = element_blank(),
-          panel.border = element_blank(),
-          plot.title = element_text(hjust = 0.5),
-          axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
-          axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black")
-          )
-
+    theme(panel.grid = element_blank())
   
-  scale <- if(is.numeric(plt_df[[annotate]])){
-    if(length(palette) == 1 && palette %in% c("viridis", "magma", "inferno", "plasma",
-                                              "cividis", "rocket", "mako", "turbo")){
-      scale_color_viridis_c(option = palette)
-    }else if(length(palette) == 1 && palette == "seuratlike"){
-      scale_color_gradientn(colors = colorRampPalette(colors = rev(x = RColorBrewer::brewer.pal(n = 11, name = "Spectral")))(100),
-                           limits = c(min(plt_df[[annotate]]), max(plt_df[[annotate]])))
-    }else{
-      scale_color_gradient(low = palette[1], high = palette[2])
+  
+  # additional plot formatting
+  
+  # color scale
+  scaling <- if (is.numeric(df[[annotate]])) {
+    # continuous values
+    if (length(pal) == 1 && 
+        palette %in% c("viridis", "magma", "inferno", "plasma", "cividis", 
+                       "rocket", "mako", "turbo")) {
+      scale_color_viridis_c(option = pal)
+    } else if (length(pal) == 1 && palette == "seuratlike") {
+      colors <- colorRampPalette(
+        colors = rev(x = brewer.pal(n = 11, name = "Spectral")))(100)
+      scale_color_gradientn(
+        colors = colorRampPalette(colors = colors), 
+        limits = range(df[[annotate]]))
+    } else {
+      scale_color_gradient(low = pal[1], high = pal[2])
     }
-  }else if(is.factor(plt_df[[annotate]])){
-    if(is.null(palette)){ # for categorical feature, automate palette
-      scale_color_manual(name = annotate,
-                        values = scales::hue_pal()(length(unique(plt_df[[annotate]])))) 
-    }else if(!is.null(palette)){
-      scale_color_manual(values = palette)
+  } else if (is.factor(df[[annotate]]) | is.character(df[[annotate]])) {
+    # discrete values
+    if (is.null(pal)) {
+      scale_color_manual(name = annotate, 
+                         values = hue_pal()(length(unique(df[[annotate]]))))
+    } else if (!is.null(pal)) {
+      scale_color_manual(values = pal)
     }
   }
   
-  
-  if(is.numeric(plt_df[[annotate]])){ # continuous display plot title but no legend title
-    p <- p + scale + ggtitle(annotate) + labs(color = NULL) 
-  }else if(is.factor(plt_df[[annotate]])){ # categorical disply legend title but no plot title
-    p <- p + scale + 
-      guides(colour = guide_legend(override.aes = list(size = 3)))
+  # plot title
+  if (is.numeric(df[[annotate]])) {
+    # continuous values: display plot title but no legend title
+    p <- p + 
+      scaling + 
+      ggtitle(annotate) + 
+      labs(color = NULL)
+  } else if (is.factor(df[[annotate]]) | is.character(df[[annotate]])) {
+    # discrete values: display legend title but no plot title
+    p <- p + 
+      scaling + 
+      guides(color = guide_legend(override.aes = list(size = 3)))
   }
   
-  
-  if(is.factor(plt_df[[annotate]])){
-    # Adding text with the median locations of the 'text_by' vector.
-    if(!is.null(text_by)){
+  # text annotations
+  if (is.factor(df[[annotate]]) | is.character(df[[annotate]])) {
+    # add text with the median locations of the 'text_by' vector
+    if (!is.null(text_by)) {
       by_text_x <- vapply(
-        split(plt_df[[paste0(type, "_1")]], plt_df[[text_by]]),
-        median,
+        split(df[[x_label]], df[[text_by]]), 
+        median, 
         FUN.VALUE = 0
       )
-      
       by_text_y <- vapply(
-        split(plt_df[[paste0(type, "_2")]], plt_df[[text_by]]),
-        median,
+        split(df[[y_label]], df[[text_by]]), 
+        median, 
         FUN.VALUE = 0
       )
-      
-      p <- p +
-        ggrepel::geom_text_repel(
+      p <- p + 
+        geom_text_repel(
           data = data.frame(
-            x = by_text_x, y = by_text_y, label = names(by_text_x)
-          ),
-          mapping = aes(x = .data$x, y = .data$y, label = .data$label),
-          size = text_by_size, colour = text_by_color
+            x = by_text_x, 
+            y = by_text_y, 
+            label = names(by_text_x)
+          ), 
+          mapping = aes(x = .data$x, y = .data$y, label = .data$label), 
+          size = text_by_size, 
+          color = text_by_color
         )
     }
   }
-
   
+  # return plot
   p
 }
-
