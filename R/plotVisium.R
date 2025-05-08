dc19aa8bf2f0caa58a51e670ff9ed920d0fdff91
Lukas Weber <lmweb012@gmail.com>
2025-05-08 14:27:32 -0400

    fix broken link in docs

--- R/plotVisium.R
+++ R/plotVisium.R
@@ -43,7 +43,7 @@
 #'   
 #' @param trans Transformation to apply for continuous scales. Ignored unless
 #'   \code{annotate} is numeric, e.g. feature expression. (See
-#'   \code{\link{ggplot2}{continuous_scale}} for valid options.)
+#'   \code{?continuous_scale} from \code{ggplot2} for valid options.)
 #' 
 #' @param point_shape (numeric) Point shape. Default = 21, which gives a
 #'   circular shape suitable for representing for example, a Visium spot or
